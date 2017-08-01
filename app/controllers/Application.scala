d5d89eac93369b68ace5e23e6bf609bddeca3e0d
TheDeadlyPianist <duane221@yahoo.co.uk>
2017-08-01 15:11:20 +0100

    Seating plan update

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,18 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import scalaEnum.seatingPlanArray._
 
 class Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def seating(seatingPlan:String) = Action {
+    val seatingObj:Map[String, Array[Int]] = Map("seats1" -> seats1, "seats2" -> seats2)
+    val useSeats:Array[Int] = seatingObj(seatingPlan)
+    Ok(views.html.seatingSystem(useSeats))
   }
 
 }
