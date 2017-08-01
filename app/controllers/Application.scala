d1eeeb2a78cfc77268fa7ca7c9939f9485a4eaa6
AlexRimmerQA <alex.rimmer@qa.com>
2017-08-01 12:21:44 +0100

    problem solving

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,10 +6,19 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Index: Success"))
+  }
 
+  def theListing = Action {
+    Ok(views.html.listing("Listing: Success"))
+  }
 
-    //test
+  def theAbout = Action {
+    Ok(views.html.about("About: Success"))
+  }
+
+  def theDeals = Action {
+    Ok(views.html.deals("Deals: Success"))
   }
 
 }
