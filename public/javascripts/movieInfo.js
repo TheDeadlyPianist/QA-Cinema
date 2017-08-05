97eb0a81fa60f5c718d8640f1fbf56fdcaed3ccf
Rut Patel <rut.patel@qa.com>
2017-08-06 00:54:38 +0100

    MovieInfo Page CSS Updated V1

--- public/javascripts/movieInfo.js
+++ public/javascripts/movieInfo.js
@@ -62,15 +62,33 @@ function getAgeRating(){
         console.log(response);
 
         var ageRating = "";
+
         for (var i = 0; i < response.results.length; i++) {
             if (response.results[i].iso_3166_1 == "GB"){
                 for (var j = 0; j  < response.results[i].release_dates.length; j ++) {
-                    ageRating += response.results[i].release_dates[j].certification;
+
+                    if(response.results[i].release_dates[j].certification == "U"){
+                        ageRating += "https://vignette4.wikia.nocookie.net/memoryalpha/images/3/37/BBFC_U.png/revision/20120614184504/scale-to-width-down/120?path-prefix=en"
+                    }else if(response.results[i].release_dates[j].certification == "PG"){
+                        ageRating += "https://jaybullimore98.files.wordpress.com/2014/12/pg.png"
+                    }else if(response.results[i].release_dates[j].certification == "12"){
+                        ageRating += "https://jaybullimore98.files.wordpress.com/2014/12/12.png"
+                    }else if(response.results[i].release_dates[j].certification == "12A"){
+                        ageRating += "https://jaybullimore98.files.wordpress.com/2014/12/12a.png"
+                    }else if(response.results[i].release_dates[j].certification == "15"){
+                        ageRating += "https://jaybullimore98.files.wordpress.com/2014/12/15.png"
+                    }else if(response.results[i].release_dates[j].certification == "18"){
+                        ageRating += "https://jaybullimore98.files.wordpress.com/2014/12/18.png"
+                    }else if(response.results[i].release_dates[j].certification == "R18"){
+                        ageRating += "http://www.erotictradeonly.com/wp-content/uploads/2014/01/LOGO_BBFC_R18-175x109.jpg"
+                    }
+
                 }
             }
         }
 
-        $('<h2 id="ageRating">'+"Age Rating: " + ageRating+'</h2></br>').appendTo('#movieInformation');
+        //$('<h2 id="ageRating">'+"Age Rating: " + ageRating+'</h2></br>').appendTo('#movieInformation');
+        $('<h2 id="ageRating">'+"Age Rating: " + '</br>' + '<img src="'+ageRating+'" id="ageRatingImg">'+'</h2>').appendTo('#movieInformation');
         $('<button id="bookTicketBtn" onclick="bookTicket()">Book</button>').appendTo('#movieInformation')
     });
 }
@@ -104,5 +122,6 @@ function getMovieTrailer(){
 function bookTicket() {
 
     alert("Book Ticket")
+
 
 }
