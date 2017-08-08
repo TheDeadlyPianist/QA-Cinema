a1ca979eaab6f701218ceba8d4d4b1b020399a8d
Megalawls <daniel.russell@jrweb.org.uk>
2017-08-08 12:57:58 +0100

    Listings fully tested

--- test/ListingsBT.scala
+++ test/ListingsBT.scala
@@ -4,6 +4,7 @@ import org.junit.runner._
 import org.openqa.selenium.By
 import play.api.test._
 import play.api.test.Helpers._
+import java.util.concurrent.TimeUnit
 
 @RunWith(classOf[JUnitRunner])
 class ListingsBT extends Specification {
@@ -24,10 +25,12 @@ class ListingsBT extends Specification {
       //Asserts that the listings button takes you to the Listings page
       browser.webDriver.getCurrentUrl mustEqual("http://localhost:9000/listing")
       //Asserts that the tabs work
-      webDriver.findElement(By.id("righttab")).click()
-      webDriver.findElement(By.id("lefttab")).click()
-      webDriver.findElement(By.id("righttab")).click()
+      webDriver.findElement(By.id("rightTab")).click()
+      webDriver.findElement(By.id("leftTab")).click()
+      webDriver.findElement(By.id("rightTab")).click()
 
+      //Ensures that the tab content loads dynamically
+      webDriver.manage.timeouts.implicitlyWait(10, TimeUnit.SECONDS)
       //Asserts that the tabs have worked correctly, and that the first movie in the list is not the same
       webDriver.findElement(By.id("imgundefined")).click()
       //Confirms that the movie clicked on in upcoming is not the one in now showing
