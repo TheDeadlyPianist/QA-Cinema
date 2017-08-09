4db8cc92eb330c368676cd11acdd4d8e053706f1
TheDeadlyPianist <duane221@yahoo.co.uk>
2017-08-09 10:44:54 +0100

    index fixed

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -43,7 +43,7 @@ class Application @Inject()(val messagesApi: MessagesApi, mailerClient: MailerCl
     }
     val moviesInDB = Await.result[Seq[Document]](pullDB, 10 seconds)
 
-    for(i <- 0 until moviesInDB.length) {
+    for(i <- 0 until moviesInDB.length if i < 4) {
       val newName = moviesInDB(i)("title").asString().getValue
       val newYear = moviesInDB(i)("year").asInt32().getValue
       val newURL = s"https://api.themoviedb.org/3/search/movie?api_key=324938bccc324fb58e236a92cb0a9bc3&language=en-US&query=$newName&page=1&include_adult=true&year=$newYear".replace(" ", "%20")
@@ -97,9 +97,10 @@ class Application @Inject()(val messagesApi: MessagesApi, mailerClient: MailerCl
     getFilmDocument.onSuccess {
       case result => result
     }
+    println("STUFFS: " + Await.result(getFilmDocument, 10 seconds))
     val seatingPlan = "screen" + Await.result(getFilmDocument, 10 seconds)(0)("screen").asInt32().intValue()
 
-    val seatingObj:Map[String, Array[Int]] = Map("screen1" -> seats1, "screen2" -> seats2)
+    val seatingObj:Map[String, Array[Int]] = Map("screen1" -> seats1, "screen2" -> seats2, "screen3" -> seats3, "screen4" -> seats4, "screen5" -> seats5, "screen6" -> seats6, "screen7" -> seats8, "screen8" -> seats8)
     var useSeats:Array[Int] = seatingObj(seatingPlan).clone()
     var letterMap: Map[Int, Char] = Map()
     val lengthOfSeats = useSeats.count(_ == 2) + 1
@@ -170,14 +171,23 @@ class Application @Inject()(val messagesApi: MessagesApi, mailerClient: MailerCl
     val cost = (retVal \ "totalCost").as[Float].toString()
 
     val inputQuery = Document("filmTitle"->filmTitle,"time"->time,"date"->date,"cost"->cost,"seats"->(for(i <- 0 until seats.length) yield {seats(i)}))
-
     val future = Future(receipts.insertOne(inputQuery).results())
     future.onSuccess{
       case result => result
     }
     Await.result(future, 10 seconds)
 
-    Ok("")
+    val searchFuture = Future(receipts.find(inputQuery).results())
+    val returnDocument = Await.result(searchFuture, 10 seconds)
+    val dbID = returnDocument(0)("_id").asObjectId().getValue.toString()
+    println("Got to this point")
+    Ok(views.html.receipt(dbID))
+  }
+
+  def receiptView(receiptID:String) = Action {
+    Ok(
+      views.html.receipt(receiptID)
+    )
   }
 
   def sendEmail(from: String, name: String, subject: String, text: String): Unit ={
